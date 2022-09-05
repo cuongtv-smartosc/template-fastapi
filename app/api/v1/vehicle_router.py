c3d40add2c73794fc14aae2d14f0a2821994a85a
cuongtv <cuongtv@smartosc.com>
2022-09-05 10:56:17 +0700

    vehicle_list_api

---
+++ app/api/v1/vehicle_router.py
@@ -0,0 +1,38 @@
+from fastapi import Depends
+from fastapi.encoders import jsonable_encoder
+from fastapi.routing import APIRouter
+from pydantic import ValidationError
+from sqlalchemy.orm import Session
+
+from app.common.database import get_db
+from app.common.handle_error import ValidateException
+from app.models.user import User
+from app.schemas.electric_vehicle import VehicleGet
+from app.schemas.response import resp
+from app.services.auth import get_current_user
+from app.services.electric_vehicle import get_vehicle_list
+
+vehicle_router = APIRouter()
+
+
+@vehicle_router.get("/")
+async def get_vehicles(
+    params: VehicleGet = Depends(),
+    current_user: User = Depends(
+        get_current_user,
+    ),
+    db: Session = Depends(get_db),
+):
+    try:
+        param_dict = jsonable_encoder(params)
+        results = get_vehicle_list(
+            filter=param_dict,
+            current_page=params.current_page,
+            page_size=params.page_size,
+            order_by=params.order_by,
+            db=db,
+            current_user=current_user,
+        )
+        return resp.success(data=results)
+    except ValidationError as e:
+        raise ValidateException(e.errors(), key_return="detail")
---
+++ app/services/electric_vehicle.py
@@ -0,0 +1,155 @@
+import json
+import math
+
+from fastapi.encoders import jsonable_encoder
+
+from app.common.util import get_company_name_from_user
+from app.models.company import Company
+from app.models.customer import Customer
+from app.models.division import Division
+from app.models.electric_vehicle import Vehicle
+from app.models.electric_vehicle_divison import VehicleDivision
+from app.models.sale_information import SaleInformation
+from app.models.work_shift import WorkShift
+from app.schemas.electric_vehicle import VehicleFilter, VehicleFilterList
+
+
+def check_role_supervisor(current_user):
+    roles = current_user.role_name
+    if not roles:
+        roles = []
+    return (
+        "SCG-Inter Administrator" in roles
+        or "SCG-Inter Fleet Manager" in roles
+        or "System Manager" in roles
+    )
+
+
+def vehicles_list_base_filter(group_by, query, db, current_user, params):
+    if check_role_supervisor(current_user):
+        company_names = params.get("company_name")
+        if company_names is not None:
+            query = query.join(Company).filter(
+                Customer.company_id == Company.id,
+                Company.name.in_(company_names),
+            )
+    else:
+        company_names = get_company_name_from_user(current_user, db)
+        query = query.join(Company).filter(
+            Customer.company_id == Company.id,
+            Company.name.in_(company_names),
+        )
+    customer_names = params.get("customer_name")
+    if customer_names is not None:
+        query = query.filter(
+            Customer.customer_name.in_(customer_names),
+        )
+    division_names = params.get("division_name")
+    if division_names is not None:
+        query = (
+            query.join(VehicleDivision)
+            .join(Division)
+            .filter(
+                Vehicle.id == VehicleDivision.vehicle_id,
+                Division.id == VehicleDivision.division_id,
+                Division.name.in_(division_names),
+            )
+        )
+    work_shifts = params.get("work_shift")
+    if work_shifts is not None:
+        query = query.join(WorkShift).filter(
+            Vehicle.id == WorkShift.vehicle_id,
+        )
+        work_shift_ids = []
+        for item in work_shifts:
+            work_shift_id = db.query(WorkShift.id).filter(
+                WorkShift.work_shift == item.get("shift"),
+                WorkShift.work_shift_from == item.get("ws_from"),
+                WorkShift.work_shift_to == item.get("ws_to"),
+            )
+            data = work_shift_id.all()
+            for ws in data:
+                work_shift_ids.append(ws[0])
+        query = query.filter(WorkShift.id.in_(work_shift_ids))
+    model_ids = params.get("model_id")
+    if model_ids is not None:
+        query = query.filter(Vehicle.model_id.in_(model_ids))
+    vehicle_numbers = params.get("vehicle_number")
+    if vehicle_numbers is not None:
+        query = query.filter(
+            Vehicle.vehicle_number.in_(vehicle_numbers),
+        )
+    operation_status = params.get("operation_status")
+    if operation_status is not None:
+        query = query.filter(
+            Vehicle.operation_status.in_(operation_status),
+        )
+    sale_id = params.get("sale_id")
+    if sale_id is not None:
+        query = query.filter(Vehicle.sale_id == sale_id)
+    sale_type = params.get("sale_type")
+    if sale_type is not None:
+        query = query.filter(Vehicle.sale_type == sale_type)
+    location = params.get("location")
+    if location is not None:
+        query = query.filter(Vehicle.location == location)
+    forklift_pdi_status = params.get("forklift_pdi_status")
+    if forklift_pdi_status is not None:
+        query = query.filter(
+            Vehicle.forklift_pdi_status == forklift_pdi_status,
+        )
+    sale_order_number = params.get("sale_order_number")
+    if sale_order_number is not None:
+        query = query.filter(
+            Vehicle.sale_id == sale_order_number,
+        )
+    if group_by is not None:
+        query = query.group_by(group_by)
+    return query
+
+
+def get_vehicle_list(
+    filter,
+    current_page,
+    page_size,
+    order_by,
+    db,
+    current_user,
+):
+    param = VehicleFilterList(**filter).dict()
+    for key in param:
+        if param[key] is not None:
+            param[key] = json.loads(param[key])
+    param.update(VehicleFilter(**filter).dict())
+    query = db.query(
+        Vehicle.vehicle_number,
+        Vehicle.id,
+        Vehicle.model_id,
+        Vehicle.edge_id,
+        Customer.customer_name,
+        SaleInformation.sale_type,
+    ).filter(
+        Vehicle.sale_id == SaleInformation.id,
+        SaleInformation.customer_id == Customer.id,
+    )
+    query = vehicles_list_base_filter(
+        Vehicle.vehicle_number, query, db, current_user, param
+    )
+    total = query.count()
+    edges = [i.edge_id for i in query.all()]
+    if order_by == "asc":
+        query = query.order_by(Vehicle.vehicle_number.asc())
+    elif order_by == "customer_name":
+        query = query.order_by(Customer.customer_name.asc())
+    else:
+        query = query.order_by(Vehicle.vehicle_number.desc())
+    offset = (current_page - 1) * page_size
+    query = query.limit(page_size).offset(offset).all()
+    results = {
+        "total": total,
+        "currentPage": current_page,
+        "totalPage": math.ceil(total / page_size),
+        "vehicles": jsonable_encoder(query),
+        "list_edge": edges,
+    }
+    return results
