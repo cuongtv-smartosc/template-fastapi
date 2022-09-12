84f71c0ae343e4381c3de5f17250a0a2f80e9d56
cuongtv-smartosc <92746585+cuongtv-smartosc@users.noreply.github.com>
2022-09-12 16:49:24 +0700

    get sale_information (#47)
    * get sale_information

---
+++ app/services/sale_information.py
@@ -0,0 +1,65 @@
+from datetime import datetime
+
+from fastapi.encoders import jsonable_encoder
+
+from app.common.handle_error import NotFoundException
+from app.common.util import check_role_supervisor, get_company_id_from_user
+from app.common.util import json_load as json_load
+from app.models.customer import Customer
+from app.models.electric_vehicle import Vehicle
+from app.models.sale_information import SaleInformation
+
+
+def get_list_coordinate(coordinates=None):
+    if not coordinates:
+        return []
+    try:
+        return json_load(coordinates)
+    except Exception:
+        return []
+
+
+def get_by_id(id, db, current_user):
+    query = db.query(
+        SaleInformation.sale_type,
+        SaleInformation.sale_order_number,
+        Customer.customer_name,
+        Customer.address,
+        SaleInformation.location,
+        Vehicle.delivering_date,
+        SaleInformation.vehicle_warranty,
+        SaleInformation.battery_warranty,
+        SaleInformation.battery_maintenance,
+        SaleInformation.service,
+        SaleInformation.start_date,
+        SaleInformation.end_date,
+        SaleInformation.coordinates,
+        SaleInformation.contract_no,
+    ).filter(
+        SaleInformation.id == Vehicle.sale_id,
+        SaleInformation.customer_id == Customer.id,
+        Vehicle.id == id,
+    )
+    if not check_role_supervisor(current_user):
+        company = get_company_id_from_user(current_user, db)
+        query = query.filter(
+            Customer.company_id.in_(company),
+        )
+    return query.first()
+
+
+def get_sale_information(id, db, current_user):
+    sale_information = jsonable_encoder(get_by_id(id, db, current_user))
+    if not sale_information:
+        raise NotFoundException(f"{id} is not existed")
+    end_date = sale_information.get("end_date")
+    sale_information["remaining_day"] = None
+    if end_date is not None:
+        now = datetime.now().date()
+        sale_information["remaining_day"] = (
+            datetime.strptime(end_date, "%Y-%m-%d").date() - now
+        ).days
+    sale_information["coordinates"] = get_list_coordinate(
+        sale_information["coordinates"]
+    )
+    return sale_information
---
+++ tests/test_sale_information.py
@@ -0,0 +1,81 @@
+from tests.base_test import BaseTestCase, get_token_for_test
+from tests.factories.customer import CustomerFactory
+from tests.factories.electric_vehicle import VehicleFactory
+from tests.factories.sale_information import SaleInformationFactory
+from tests.factories.user import UserFactory
+
+
+class GetSaleInformationTestCase(BaseTestCase):
+    def setUp(self) -> None:
+        super().setUp()
+        user = UserFactory.create()
+        global user2
+        user2 = UserFactory.create(username="guest", role_name="")
+        token = get_token_for_test(username=user.username)
+        self.client.headers = {"Authorization": f"Bearer {token}"}
+        customer = CustomerFactory.create(customer_name="abc", system_user="2")
+        sale_if = SaleInformationFactory.create(customer_id=customer.id)
+        VehicleFactory.create_batch(3, sale_id=sale_if.id)
+        VehicleFactory.create_batch(25)
+
+    def test_get_sale_information_success(self):
+        vehicle_id = 1
+        response = self.client.get(
+            f"/api/electric_vehicle/{vehicle_id}/sale_information",
+        )
+        res = response.json()
+        data = res.get("data")
+        assert response.status_code == 200
+        assert res["msg"] == "success"
+        assert data.get("customer_name") == "abc"
+
+    def test_get_sale_information_validate_id_is_int(self):
+        vehicle_id = "abc"
+        response = self.client.get(
+            f"/api/electric_vehicle/{vehicle_id}/sale_information",
+        )
+        res = response.json()
+        data = res.get("message")
+        assert response.status_code != 200
+        assert response.status_code == 422
+        assert data[0].get("msg") == "value is not a valid integer"
+        assert data[0].get("loc")[0] == "path"
+        assert data[0].get("loc")[1] == "id"
+        assert data[0].get("type") == "type_error.integer"
+
+    def test_get_sale_information_not_found(self):
+        vehicle_id = 29
+        response = self.client.get(
+            f"/api/electric_vehicle/{vehicle_id}/sale_information",
+        )
+        res = response.json()
+        data = res.get("message")
+        assert response.status_code != 200
+        assert response.status_code == 404
+        assert data == f"{vehicle_id} is not existed"
+
+    def test_get_information_by_user_company_not_found(self):
+        vehicle_id = 5
+        token = get_token_for_test(username=user2.username)
+        self.client.headers = {"Authorization": f"Bearer {token}"}
+        response = self.client.get(
+            f"/api/electric_vehicle/{vehicle_id}/sale_information",
+        )
+        res = response.json()
+        data = res.get("message")
+        assert response.status_code != 200
+        assert response.status_code == 404
+        assert data == f"{vehicle_id} is not existed"
+
+    def test_get_sale_information_by_user_company_success(self):
+        vehicle_id = 3
+        token = get_token_for_test(username=user2.username)
+        self.client.headers = {"Authorization": f"Bearer {token}"}
+        response = self.client.get(
+            f"/api/electric_vehicle/{vehicle_id}/sale_information",
+        )
+        res = response.json()
+        data = res.get("data")
+        assert response.status_code == 200
+        assert res["msg"] == "success"
+        assert data.get("customer_name") == "abc"
